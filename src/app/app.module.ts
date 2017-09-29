c0c3ae134dbda50b2d30067c14245650898b2ecd
maxcassettes <mattcossette@gmail.com>
2017-09-28 21:21:04 -0400

    Version 1.0
    -Facebook authentication
    -Add/Update/Delete task

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import {AngularFireModule} from 'angularfire2'; 
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth'; 
+
+import {Facebook} from '@ionic-native/facebook';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1o8rcqUOA8um4RDs8LKYBXqP2nz4eISI",
+  authDomain: "auth-ng4-ionic-aft2.firebaseapp.com",
+  databaseURL: "https://auth-ng4-ionic-aft2.firebaseio.com/",
+  storageBucket: "gs://auth-ng4-ionic-aft2.appspot.com",
+  messagingSenderId: '522892925115'
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp), 
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, 
+    Facebook
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,158 @@
+import { Component } from '@angular/core';
+import { NavController, 
+  AlertController, 
+  ActionSheetController } from 'ionic-angular';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+import { Platform } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+items: FirebaseListObservable<any[]>; 
+
+
+  displayName;
+  userID; 
+  tasks;  
+
+  constructor(public navCtrl: NavController,
+    private afAuth: AngularFireAuth, private fb: Facebook, private platform: Platform, afDB: AngularFireDatabase, public alertCtrl: AlertController, public actionCtrl: ActionSheetController) {
+
+    afAuth.authState.subscribe((user: firebase.User) => {
+      if (!user) {
+        this.displayName = 'Please log in';
+        return;
+      }
+      //get the username and id
+      this.displayName = user.displayName;   
+      this.userID = user.uid;
+      //put them in the database for reference
+     firebase.database().ref('/users/' + this.userID).update({displayName: this.displayName}); 
+     this.tasks = afDB.list('/users/' + this.userID + '/tasks');
+     console.log(this.tasks);
+    });
+  }
+
+  signInWithFacebook() {
+    if (this.platform.is('cordova')) {
+      return this.fb.login(['email', 'public_profile']).then(res => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+        return firebase.auth().signInWithCredential(facebookCredential)
+        ;
+      })
+    }
+    else {
+      return this.afAuth.auth
+        .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+        .then(res => console.log(res));
+    }
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+    this.tasks = "";
+  }
+ 
+addTask() {
+ let prompt = this.alertCtrl.create({
+  title: 'New Task', 
+  message: '', 
+  inputs: [
+  {
+    name: 'title', 
+    placeholder: 'Task'
+  },
+  ],
+  buttons: [
+  {
+    text: 'Cancel', 
+    handler: data => {
+      console.log('Cancel clicked'); 
+    }
+  },
+  {
+    text: 'Save', 
+    handler: data => {
+      this.tasks.push({
+        title: data.title
+      });
+    }
+  }
+  ]
+ });
+ prompt.present();
+}
+
+
+showOptions(taskId, taskTitle) {
+let actionSheet = this.actionCtrl.create({
+  title: 'What do you want to do?', 
+  buttons: [
+  {
+    text: 'Delete task', 
+    role: 'destructive', 
+    handler: () => {
+      this.removeTask(taskId); 
+    }
+  },
+  {
+    text: 'Update task', 
+    handler: () => {
+      this.updateTask(taskId, taskTitle);
+    }
+  },
+  {
+    text: 'Cancel', 
+    handler:() => {
+      console.log('Cancel Clicked'); 
+    }
+  }
+  ]
+});
+actionSheet.present();
+}
+
+removeTask(taskId: string) {
+  this.tasks.remove(taskId); 
+}
+
+updateTask(taskId, taskTitle){
+  let prompt = this.alertCtrl.create({
+    title: 'Update',
+    message: "",
+    inputs: [
+      {
+        name: 'title',
+        placeholder: '',
+        value: taskTitle
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.tasks.update(taskId, {
+            title: data.title
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+}
